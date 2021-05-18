b00fdf4cc1fe34cce8ba04be497f0668333c39f3
Bittbom <bittbom@kakao.com>
2021-05-18 14:56:47 +0900

    프로필 이미지 수정 1

--- src/components/ChatPage/SidePanel/UserPanel.js
+++ src/components/ChatPage/SidePanel/UserPanel.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useSelector } from "react-redux";
 import { IoIosChatboxes } from "react-icons/io";
 import Dropdown from "react-bootstrap/Dropdown";
 import Image from "react-bootstrap/Image";
-import firebase from "../../../firebase"
+import firebase from "../../../firebase";
 
 function UserPanel() {
   const user = useSelector((state) => state.user.currentUser);
-
+  const inputOpenImageRef = useRef();
   const handleLogout = () => {
     firebase.auth().signOut();
   };
+  const handleOpenImageRef = () => {
+    inputOpenImageRef.current.click();
+  };
 
+  const handleUploadImage = () => {
+     
+  }
   return (
     <div>
       <h3 style={{ color: "white" }}>
@@ -33,15 +39,24 @@ function UserPanel() {
             style={{ background: "transparent", border: "0px" }}
             id="dropdown-basic"
           >
-            {user.displayName}
+            {user && user.displayName}
           </Dropdown.Toggle>
 
           <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">프로필 사진 변경</Dropdown.Item>
+            <Dropdown.Item onClick={handleOpenImageRef}>
+              프로필 사진 변경
+            </Dropdown.Item>
             <Dropdown.Item onClick={handleLogout}>로그아웃</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
       </div>
+      <input
+        onChange={handleUploadImage}
+        accept="image/jpeg, image/png"
+        style={{ display: "none" }}
+        ref={inputOpenImageRef}
+        type="file"
+      />
     </div>
   );
 }
