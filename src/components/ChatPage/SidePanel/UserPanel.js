420c958d4e05d834d45dcfdd5d9d57e820cc8fc8
Bittbom <bittbom@kakao.com>
2021-05-18 16:25:54 +0900

    프로필 이미지 수정(3) 및 리덕스 스토어 데이터 변경 및 데이타베이스에 데이터 저장

--- src/components/ChatPage/SidePanel/UserPanel.js
+++ src/components/ChatPage/SidePanel/UserPanel.js
@@ -1,75 +1,97 @@
-import React, { useRef } from "react";
-import { useSelector } from "react-redux";
-import { IoIosChatboxes } from "react-icons/io";
-import Dropdown from "react-bootstrap/Dropdown";
-import Image from "react-bootstrap/Image";
-import firebase from "../../../firebase";
-import mime from "mime-types";
+import React, { useRef } from 'react'
+import { IoIosChatboxes } from 'react-icons/io';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Image from 'react-bootstrap/Image';
+import { useDispatch, useSelector } from 'react-redux';
+import firebase from '../../../firebase';
+import mime from 'mime-types';
+import { setPhotoURL } from '../../../redux/actions/user_action';
+
 
 function UserPanel() {
-  const user = useSelector((state) => state.user.currentUser);
-  const inputOpenImageRef = useRef();
-  const handleLogout = () => {
-    firebase.auth().signOut();
-  };
-  const handleOpenImageRef = () => {
-    inputOpenImageRef.current.click();
-  };
+    const user = useSelector(state => state.user.currentUser)
+    const dispatch = useDispatch();
+    const inputOpenImageRef = useRef();
 
-  const handleUploadImage = async (event) => {
-    const file = event.target.files[0];
-    const metadata = { contentType: mime.lookup(file.name) };
+    const handleLogout = () => {
+        firebase.auth().signOut();
+    }
 
-    try {
-      let uploadTaskSnapshot = await firebase
-        .storage()
-        .ref()
-        .child(`user_image/${user.uid}`)
-        .put(file, metadata);
-      console.log("uploadTaskSnapshot", uploadTaskSnapshot);
-    } catch (error) {}
-  };
-  return (
-    <div>
-      <h3 style={{ color: "white" }}>
-        <IoIosChatboxes /> Chap App
-      </h3>
-      <div style={{ display: "flex", marginBottom: "1rem" }}>
-        <Image
-          src={user && user.photoURL}
-          style={{
-            width: "30px",
-            height: "30px",
-            marginTop: "3px",
-          }}
-          roundedCircle
-        />
+    const handleOpenImageRef = () => {
+        inputOpenImageRef.current.click();
+    }
 
-        <Dropdown>
-          <Dropdown.Toggle
-            style={{ background: "transparent", border: "0px" }}
-            id="dropdown-basic"
-          >
-            {user && user.displayName}
-          </Dropdown.Toggle>
+    const handleUploadImage = async (event) => {
+        const file = event.target.files[0];
 
-          <Dropdown.Menu>
-            <Dropdown.Item onClick={handleOpenImageRef}>
-              프로필 사진 변경
-            </Dropdown.Item>
-            <Dropdown.Item onClick={handleLogout}>로그아웃</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </div>
-      <input
-        onChange={handleUploadImage}
-        accept="image/jpeg, image/png"
-        style={{ display: "none" }}
-        ref={inputOpenImageRef}
-        type="file"
-      />
-    </div>
-  );
+        const metadata = { contentType: mime.lookup(file.name) };
+
+        try {
+            //스토리지에 파일 저장하기 
+            let uploadTaskSnapshot = await firebase.storage().ref()
+                .child(`user_image/${user.uid}`)
+                .put(file, metadata)
+
+            let downloadURL = await uploadTaskSnapshot.ref.getDownloadURL();
+
+            // 프로필 이미지 수정
+            await firebase.auth().currentUser.updateProfile({
+                photoURL: downloadURL
+            })
+
+            dispatch(setPhotoURL(downloadURL))
+
+            //데이터베이스 유저 이미지 수정
+            await firebase.database().ref("users")
+                .child(user.uid)
+                .update({ image: downloadURL })
+
+            // console.log('uploadTaskSnapshot', uploadTaskSnapshot)
+
+        } catch (error) {
+            alert(error)
+        }
+    }
+
+    return (
+        <div>
+            {/* Logo */}
+            <h3 style={{ color: 'white' }}>
+                <IoIosChatboxes />{" "} Chat App
+            </h3>
+
+            <div style={{ display: 'flex', marginBottom: '1rem' }}>
+                <Image src={user && user.photoURL}
+                    style={{ width: '30p', height: '30px', marginTop: '3px' }}
+                    roundedCircle />
+
+                <Dropdown>
+                    <Dropdown.Toggle
+                        style={{ background: 'transparent', border: '0px' }}
+                        id="dropdown-basic">
+                        {user && user.displayName}
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item onClick={handleOpenImageRef}>
+                            프로필 사진 변경
+                        </Dropdown.Item>
+                        <Dropdown.Item onClick={handleLogout}>
+                            로그아웃
+                        </Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </div>
+
+            <input
+                onChange={handleUploadImage}
+                accept="image/jpeg, image/png"
+                style={{ display: 'none' }}
+                ref={inputOpenImageRef}
+                type="file"
+            />
+        </div>
+    )
 }
 
-export default UserPanel;
+export default UserPanel
